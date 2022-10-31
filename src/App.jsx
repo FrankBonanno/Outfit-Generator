0e399c69929345a85e69541f328b6124a6b7d80d
FrankBonanno <fb0023@mix.wvu.edu>
2022-10-31 08:04:38 -0400

    added react router
    - added react-router-dom dependency
    - Created login page
    TODO: set up firebase auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Login from './pages/Login';
 
 function App() {
 	return (
-		<div className="App">
-			<Navbar />
-		</div>
+		<Routes>
+			<Route path="/" element={<Login />} />
+			<Route path="/dashboard" element={<Navbar />} />
+		</Routes>
 	);
 }
 
